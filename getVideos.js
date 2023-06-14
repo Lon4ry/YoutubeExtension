ee8416adb099d169ac158e3ecc58f8525eead3fc
Даня <fiackopurple2018@gmail.com>
2023-06-14 15:28:26 +0300

    try catch performed

--- getVideos.js
+++ getVideos.js
@@ -1,10 +1,14 @@
 setTimeout(() => {
     const videos = document.querySelectorAll('ytd-video-renderer');
     const formattedVideos = Array.from(videos).map(video => {
-        let percent = video.querySelector('#progress').style.width
-        percent = Number(percent.slice(0, percent.length - 1))
-        const author = video.querySelector('#channel-name').querySelector('#text').textContent
-        if (percent <= 75) return {author: author, href: video.querySelector('a').href}
+        try {
+            let percent = video.querySelector('#progress').style.width
+
+            percent = Number(percent.slice(0, percent.length - 1))
+            const author = video.querySelector('#channel-name').querySelector('#text').textContent
+            if (percent <= 75) return {author: author, href: video.querySelector('a').href}
+        } catch (e) {
+        }
         return null
     })
     localStorage.setItem('videos', JSON.stringify(formattedVideos))
